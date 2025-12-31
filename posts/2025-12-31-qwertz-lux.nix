6db5e82a778191d490a46b13f58d845a16c9d455
Bruno Rodrigues <bruno@brodrigues.co>
2025-12-31 14:32:23 +0100

    feat: add QWERTZ-LUX blog post with per-post Nix dependencies
    - Converted lbkeyboard introduction vignette to blog post
    - Added post-specific .nix file for lbkeyboard and ggkeyboard
    - Layouts are fetched dynamically from GitHub at render time
    - Added posts/layouts to gitignore

---
+++ posts/2025-12-31-qwertz-lux.nix
@@ -0,0 +1,48 @@
+{ pkgs }:
+let
+  ggkeyboard = pkgs.rPackages.buildRPackage {
+    name = "ggkeyboard";
+    src = pkgs.fetchgit {
+      url = "https://github.com/b-rodrigues/ggkeyboard";
+      rev = "main";
+      sha256 = "sha256-Bd3QYtVC4dDdk4/EccCmxgL2QegmwvAhHWATuZVGM/I=";
+    };
+    propagatedBuildInputs = with pkgs.rPackages; [
+      dplyr
+      ggplot2
+      stringr
+      prismatic
+      rlang
+      ggforce
+      purrr
+    ];
+  };
+
+  lbkeyboard = pkgs.rPackages.buildRPackage {
+    name = "lbkeyboard";
+    src = pkgs.fetchgit {
+      url = "https://github.com/b-rodrigues/lbkeyboard";
+      rev = "main";
+      sha256 = "sha256-LaJBLzdfxyjeZLmQP0m38z8ZY3QJHSqnDIbSMmrhhQk=";
+    };
+    propagatedBuildInputs = with pkgs.rPackages; [
+      dplyr
+      ggplot2
+      stringr
+      tidyr
+      purrr
+      rlang
+      cli
+      Rcpp
+      GA
+      ggkeyboard
+    ];
+  };
+in
+[
+  lbkeyboard
+  ggkeyboard
+  pkgs.rPackages.dplyr
+  pkgs.rPackages.ggplot2
+]
+
